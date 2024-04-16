8bbf9713fe236bb35401df6a6adb3f46e8ed92a4
cassiano-sena <csdscr@gmail.com>
2024-04-16 17:27:16 -0300

    Update multithread-linux.c

--- multithread-linux.c
+++ multithread-linux.c
@@ -1,32 +1,3 @@
-/*
-Uma Indústria de Alimentos de Santa Catarina chamada FoodSec S.A. possui a tarefa de escanear
-alimentos por meio de câmeras e verificar se os mesmos estão corretos. Os alimentos podem passar
-por uma das duas esteiras disponíveis. As duas esteiras são controladas por um por um único
-computador centralizado. Esse computador recebe dados de um sensor em cada uma das esteiras que
-captura a contagem de itens que são identificados como seguros. A contagem é exibida em um display
-perto das esteiras (todos os displays são controlados pela mesma função, é apenas uma replicação)
-
-A empresa está avaliando a viabilidade de implementação nos Sistemas Operacionais Windows e Linux
-(qualquer distro) e também em duas formas de IPC. Com isso, você deve implementar duas versões
-da solução nos dois sistemas operacionais e comparar qual é a que apresenta o melhor desempenho
-quanto ao tempo de computação (tempo de processamento). A primeira solução deve usar IPC do tipo
-pipe (de preferência nomeado), já a segunda solução deve ser implementada via multithread (ou multithread, como queira descrever). Logo, deve ser implementado as duas soluções para Windows e as
-duas para Linux. Você poderá usar o Codespace ou Cocalc, por exemplo, para implementar a versão
-em Linux. Para Windows, você pode usar máquinas virtuais também.
-
-Os códigos disponibilizados pelo professor (de teste e exercícios) podem ser usados para o
-desenvolvimento do trabalho (na verdade, é extremamente recomendado). A avaliação do tempo deve
-ser entre a execução da contagem e na comunicação, sendo recomendado desconsiderar a criação de
-threads, pipe ou memória compartilhada.
-
-() revisar se atende ao que foi pedido
-() revisar formatacao
-() revisar funcoes
-() revisar versao linux
-() revisar versao windows
-() fazer o tratamento de spinlock
-*/
-
 // implementação multithread em linux
 #include <pthread.h>
 #include <stdio.h>
@@ -40,7 +11,8 @@ double peso_total_esteira2 = 0;
 double peso_total_combinado = 0;
 int itens_lidos = 0;
 int atualizacoes_display = 0;
-int stop_threshold = 50; // Threshold for stopping the program
+int threshold = 10;      // total de itens lidos para atualizar o display
+int stop_threshold = 50; // Limite de itens lidos para encerrar
 
 void atualiza_sensor_esteira1();
 void atualiza_sensor_esteira2();
@@ -55,7 +27,7 @@ int main()
 
     if (pthread_mutex_init(&exclusao_mutua, NULL) != 0)
     {
-        printf("\nMutex initialization failed\n");
+        printf("\nFalha na inicialização do mutex!\n");
         return 1;
     }
 
@@ -68,7 +40,8 @@ int main()
     // observador que detecta quando o numero de itens passou do limite estabelecido para encerramento
     while (itens_lidos < stop_threshold)
     {
-        sleep(1);
+        usleep(100000);
+        // sleep(1);
     }
 
     pthread_join(t1, NULL);
@@ -133,7 +106,6 @@ void atualiza_sensor_esteira2()
 
 void atualiza_display()
 {
-    int threshold = 10; // total de itens lidos para atualizar o display
     while (itens_lidos < stop_threshold)
     {
         pthread_mutex_lock(&exclusao_mutua);
